be12340c7a9c3ebb27724472dd3c4f4de19c7ba9
Noah Silbert <noahpoah@gmail.com>
2020-10-29 16:03:48 -0400

    initial commit, nothing substantive yet

---
+++ lev_heb.py
@@ -0,0 +1,7 @@
+import Levenshtein as lev
+
+# architecture notes
+# read in word list for which OLD20 stats are wanted
+# read in lexicon (surface only? base? both? input spec'd?
+# loop through words, calculate OLD20
+# write to output file
